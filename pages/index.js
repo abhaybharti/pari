7bfb830c6899f543c3a4d551933d7554eb881e20
Abhay Bharti <abhaybharti@users.noreply.github.com>
2023-11-01 22:28:18 +0530

    fixed build error

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,31 @@
-import axios from "axios";
-import { useContext } from "react";
-import { toast } from "react-toastify";
+// import axios from "axios";
+// import { useContext } from "react";
+// import { toast } from "react-toastify";
 import Layout from "../components/Layout";
 import ProductItem from "../components/ProductItem";
 import Product from "../models/Product";
 import db from "../utils/db";
-import { Store } from "../utils/Store";
+// import { Store } from "../utils/Store";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import Link from "next/link";
 
-
 export default function Home({ products, featuredProducts }) {
-  const { state, dispatch } = useContext(Store);
-  const { cart } = state;
+  // const { state, dispatch } = useContext(Store);
+  // const { cart } = state;
 
-  const addToCartHandler = async (product) => {
-    const existItem = cart.cartItems.find((x) => x.slug === product.slug);
-    const quantity = existItem ? existItem.quantity + 1 : 1;
-    const { data } = await axios.get(`/api/products/${product._id}`);
+  // const addToCartHandler = async (product) => {
+  //   const existItem = cart.cartItems.find((x) => x.slug === product.slug);
+  //   const quantity = existItem ? existItem.quantity + 1 : 1;
+  //   const { data } = await axios.get(`/api/products/${product._id}`);
 
-    if (data.countInStock < quantity) {
-      return toast.error("Sorry. Product is out of stock");
-    }
-    dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
+  //   if (data.countInStock < quantity) {
+  //     return toast.error("Sorry. Product is out of stock");
+  //   }
+  //   dispatch({ type: "CART_ADD_ITEM", payload: { ...product, quantity } });
 
-    toast.success("Product added to the cart");
-  };
+  //   toast.success("Product added to the cart");
+  // };
 
   return (
     <Layout title="Home Page">
@@ -54,11 +53,7 @@ export default function Home({ products, featuredProducts }) {
       {/* <h2 className="h-2 my-0">Latest Products</h2> */}
       <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4 mx-1 pt-2">
         {products.map((product) => (
-          <ProductItem
-            product={product}
-            key={product.slug}
-            addToCartHandler={addToCartHandler}
-          ></ProductItem>
+          <ProductItem product={product} key={product.slug}></ProductItem>
         ))}
       </div>
     </Layout>
